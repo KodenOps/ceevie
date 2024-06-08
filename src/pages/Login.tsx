76f22bbbaceb8c5b26b88c5a603bd844d23578f1
kodeninj <femifadiya.segun@gmail.com>
2024-06-09 00:30:15 +0100

    updated the register page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,11 +2,17 @@ import Navbar from '@/components/Navbar';
 import Image from 'next/image';
 import React from 'react';
 import loginImg from '../../public/asset/login_img.jpg';
+import Pattern from '../../public/asset/pattern.png';
 const Login = () => {
 	return (
-		<div>
+		<div className='relative'>
+			<Image
+				src={Pattern}
+				alt='pattern'
+				className='absolute top-0 left-0 w-[100%] -z-10 h-[100vh]'
+			/>
 			<Navbar />
-			<div className='loginWrapper md:w-[60vw] w-full h-[70vh] md:border-[1px] border-[#e8e8e8] md:mx-[20%]   rounded-lg flex items-start overflow-hidden'>
+			<div className='loginWrapper md:w-[60vw] w-full h-[70vh] md:border-[1px] border-[#e8e8e8] md:mx-[20%]   rounded-lg flex items-start overflow-hidden z-50 md:bg-white'>
 				{/* the left side of login */}
 				<div className='img w-[40%]  h-full md:block hidden'>
 					<Image
