27ea75294ce37543be36201361b1c4ff5a02a7bb
kodeninj <femifadiya.segun@gmail.com>
2024-06-08 22:49:15 +0100

    updated the navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,107 @@
+'use client';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Logo from '../../public/asset/logo.png';
+import {
+	MdArrowRight,
+	MdCall,
+	MdClose,
+	MdHome,
+	MdInfo,
+	MdMenu,
+} from 'react-icons/md';
+import { PiUserCircleCheckThin } from 'react-icons/pi';
+const Navbar = () => {
+	const [navOpen, setnavOpen] = useState(true);
+	return (
+		<nav className='flex items-center justify-between md:px-[40px] px-[16px] py-[24px]'>
+			<div className='logo'>
+				<Image
+					className='md:w-[25%] w-[20%]'
+					src={Logo}
+					// width={50}
+					alt=''
+				/>
+			</div>
+			{/* nav links for web */}
+			<div className='navLinks'>
+				<ul className='md:flex gap-8 justify-center items-center hidden text-lg'>
+					<li className='navLinks hover:font-medium'>Home</li>
+					<li className='navLinks hover:font-medium'>About</li>
+					<li className='navLinks hover:font-medium'>Supports</li>
+					<li className='navLinks w-[150px] border-2 border-solid border-[var(--primary)] text-[var(--primary)]  rounded  '>
+						Login
+					</li>
+				</ul>
+				{/* Nav Links for Mobile */}
+				<div
+					className='burgerIcon md:hidden block'
+					onClick={() => setnavOpen(!navOpen)}>
+					<MdMenu
+						size={40}
+						color='#066e9a'
+					/>
+				</div>
+			</div>
+			{/* side nav for mobile */}
+			{navOpen ? (
+				''
+			) : (
+				<div className='absolute h-[100vh] w-[100vw] bg-[white] top-0 left-0'>
+					<div className='logo w-full flex justify-between items-center py-[24px] border-b-2 px-[16px]'>
+						<Image
+							className='w-[10%]'
+							src={Logo}
+							// width={50}
+							alt=''
+						/>
+						<div onClick={() => setnavOpen(!navOpen)}>
+							<MdClose
+								size={30}
+								color='red'
+							/>
+						</div>
+					</div>
+					{/* the mobile Links */}
+					<ul className='mobileUl'>
+						<li className=''>
+							<div className='content flex justify-start items-center gap-[24px]'>
+								<span className='w-[35px] h-[35px] bg-[#6D69FA] rounded flex justify-center items-center text-white'>
+									<MdHome size={20} />
+								</span>
+								<p>Home</p>
+							</div>
+							<MdArrowRight size={20} />
+						</li>
+						<li className=''>
+							<div className='content flex justify-start items-center gap-[24px]'>
+								<span className='w-[35px] h-[35px] bg-[#6D69FA] rounded flex justify-center items-center text-white'>
+									<MdInfo size={20} />
+								</span>
+								<p>About</p>
+							</div>
+							<MdArrowRight size={20} />
+						</li>
+						<li className=''>
+							<div className='content flex justify-start items-center gap-[24px]'>
+								<span className='w-[35px] h-[35px] bg-[#6D69FA] rounded flex justify-center items-center text-white'>
+									<MdCall size={20} />
+								</span>
+								<p>Support</p>
+							</div>
+							<MdArrowRight size={20} />
+						</li>
+					</ul>
+					{/* end of top links */}
+					<div className='absolute bottom-0 h-[40vh] bg-[#f3f3f3] w-[96%] mx-[2%] rounded mb-1 px-[16px] py-[16px]'>
+						<div className='user'>
+							<PiUserCircleCheckThin size={30} />
+						</div>
+					</div>
+				</div>
+			)}
+		</nav>
+	);
+};
+
+export default Navbar;
---
+++ src/data/userDB.tsx
@@ -0,0 +1,37 @@
+
+export const userDB = {
+	username: '',
+	password: '',
+	firstname: '',
+	lastname: '',
+	gender: '',
+	email: '',
+	eduLevel: '',
+	mobileNum: '',
+	addr: '',
+	secondarySchool: {
+		name: '',
+		CertificateHeld: '',
+		startDate: '',
+		endDate: '',
+	},
+	higherInstitution: {
+		name: '',
+		CertificateHeld: '',
+		startDate: '',
+		endDate: '',
+	},
+	role: [
+		{
+			orgnization: '',
+			role: '',
+			startDate: '',
+			endDate: '',
+			achievementList: [],
+		},
+	],
+	skills: [],
+	Certification: [{ name: '', year: '' }],
+	linkenIn: '',
+	portfolio: '',
+};
