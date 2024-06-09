151a15ab8710d9366d3c8868ec59d458c4dde108
kodeninj <femifadiya.segun@gmail.com>
2024-06-09 11:22:44 +0100

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,21 +8,25 @@ interface Props {
 	userinfo: UserInfo;
 }
 const Home: React.FC<Props> = ({ userinfo }) => {
-	return (
-		<div>
-			<Navbar />
-			<div className='loginWrapper md:w-[70vw] w-full lg:h-[80vh] md:h-[75vh] md:border-[2px] border-[#e8e8e8] md:mx-[15%]   rounded-lg flex items-start overflow-hidden z-50 md:bg-white px-[24px] py-[24px]'>
-				<div className='greetUser flex items-center gap-[16px] md:justify-start justify-center w-full'>
-					<span className='w-[35px] h-[35px] bg-[#6D69FA] rounded flex justify-center items-center text-white'>
-						<FaRegSmileBeam size={20} />
-					</span>
-					<div className='text md:text-3xl text-xl font-bold'>
-						Nice Meeting you, {userinfo.fname}
+	if (userinfo) {
+		return (
+			<div>
+				<Navbar />
+				<div className='loginWrapper md:w-[70vw] w-full lg:h-[80vh] md:h-[75vh] md:border-[2px] border-[#e8e8e8] md:mx-[15%]   rounded-lg flex items-start overflow-hidden z-50 md:bg-white px-[24px] py-[24px]'>
+					<div className='greetUser flex items-center gap-[16px] md:justify-start justify-center w-full'>
+						<span className='w-[35px] h-[35px] bg-[#6D69FA] rounded flex justify-center items-center text-white'>
+							<FaRegSmileBeam size={20} />
+						</span>
+						<div className='text md:text-3xl text-xl font-bold'>
+							Nice Meeting you, {userinfo.fname}
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		('');
+	}
 };
 
 export default Home;
